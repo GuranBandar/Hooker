070dd45d063810026b8cad65ca108a990e7903e4
Göran Härdelin <goran.hardelin@outlook.com>
2022-10-31 12:11:31 +0000

    Fixat att Portugalgolfare är ett null värde

--- Affärslager/SpelareAktivitet.cs
+++ Affärslager/SpelareAktivitet.cs
@@ -38,7 +38,8 @@ namespace Hooker.Affärslager
                 spelare.Kön = spelareDS.Spelare[0].Kon;
                 spelare.Revisionsdatum = spelareDS.Spelare[0].RevisionsDatum;
                 spelare.UppdatDatum = spelareDS.Spelare[0].UppdatDatum;
-                spelare.Portugalgolfare = spelareDS.Spelare[0].Portugalgolfare;
+                spelare.Portugalgolfare = (spelareDS.Spelare[0].IsPortugalgolfareNull())
+                    ? string.Empty : spelareDS.Spelare[0].Portugalgolfare;
 
                 if (!spelareDS.Spelare[0].IsGolfklubbNrNull())
                 {
