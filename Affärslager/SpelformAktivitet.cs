740c2b89a2d609a85505a54831d02fe363409325
Göran Härdelin <goran.hardelin@outlook.com>
2024-11-26 17:12:58 +0000

    Nytt namn på objkektet Spelform och nya fält

---
+++ Affärslager/SpelformAktivitet.cs
@@ -0,0 +1,128 @@
+﻿using Hooker.Affärsobjekt;
+using Hooker.Datalager;
+using Hooker.Dataset;
+using Hooker.Gemensam;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Hooker.Affärslager
+{
+    /// <summary>
+    /// Klass för Spelform
+    /// 
+    /// Innehåller alla metoder för klassen Spelforms verksamhetslogik.
+    /// </summary>
+    public sealed class SpelformAktivitet : SökVillkor
+    {
+        /// <summary>
+        /// Hämtar rad från tabellen Spelform i aktuell databas med angiven nyckel.
+        /// </summary>
+        /// <param name="SpelformID">Aktuell spelform</param>
+        /// <returns>Objekt med efterfrågat data</returns>
+        public Spelform HämtaSpelform(int SpelformID)
+        {
+            SpelformData SpelformData = new SpelformData();
+            SpelformDS SpelformDS = SpelformData.HämtaSpelform(SpelformID);
+            Spelform Spelform = null;
+
+            if (SpelformDS.Spelform.Count == 1)
+            {
+                //Skapa Spelformobjekt
+                Spelform = new Spelform();
+                Spelform.SpelformID = SpelformDS.Spelform[0].SpelformID;
+                Spelform.Titel = SpelformDS.Spelform[0].Titel;
+                Spelform.Beskrivning = SpelformDS.Spelform[0].Beskrivning;
+                Spelform.Lagspel = SpelformDS.Spelform[0].Lagspel;
+                Spelform.AntalPerLag = (SpelformDS.Spelform[0].IsAntalPerLagNull()) ? string.Empty : SpelformDS.Spelform[0].AntalPerLag;
+                Spelform.AnvandarNamnSkapad = SpelformDS.Spelform[0].AnvandarNamnSkapad;
+                Spelform.SkapadDatum = SpelformDS.Spelform[0].SkapadDatum;
+                Spelform.AnvandarNamnUppdat = SpelformDS.Spelform[0].AnvandarNamnUppdat;
+                Spelform.UppdatDatum = SpelformDS.Spelform[0].UppdatDatum;
+            }
+            return Spelform;
+        }
+
+        /// <summary>
+        /// Hämtar alla rader från tabellen Spelform i aktuell databas.
+        /// </summary>
+        /// <returns>Objekt med efterfrågat data</returns>
+        public List<Spelform> HämtaAllaSpelformer()
+        {
+            DataSet SpelformDS = new DataSet();
+            SpelformData SpelformData = new SpelformData();
+            SpelformDS = SpelformData.HämtaAllaSpelformer();
+            List<Spelform> Spelform = new List<Spelform>();
+
+            if (SpelformDS.Tables[0].Rows.Count > 0)
+            {
+                Spelform = new List<Spelform>(SpelformDS.Tables[0].Rows.Count);
+                foreach (DataRow rad in SpelformDS.Tables[0].Rows)
+                {
+                    //Skapa SpelformObjektet
+                    Spelform.Add(new Spelform()
+                    {
+                        SpelformID = (int)rad["SpelformID"],
+                        Titel = rad["Titel"].ToString(),
+                        Beskrivning = rad["Beskrivning"].ToString(),
+                        Lagspel = rad["Lagspel"].ToString(),
+                        AntalPerLag = rad["AntalPerLag"].ToString(),
+                        AnvandarNamnSkapad = rad["AnvandarNamnSkapad"].ToString(),
+                        SkapadDatum = rad["SkapadDatum"].ToString(),
+                        AnvandarNamnUppdat = rad["AnvandarNamnUppdat"].ToString(),
+                        UppdatDatum = rad["UppdatDatum"].ToString(),
+                    });
+                }
+            }
+
+            return Spelform;
+        }
+
+        /// <summary>
+        /// Sparar alla förändringar i Spelform i databasen 
+        /// </summary>
+        /// <param name="Spelform">Aktuell spelform</param>
+        /// <param name="nySpelform">Ny Spelform, true or false</param>
+        /// <param name="felID">Felmeddelande i Ordlistan som ska visas</param>
+        /// <param name="feltext">Ev kompletterande felmeddelande som returneras</param>
+        public int Spara(Spelform Spelform, bool nySpelform, ref string felID, ref string feltext)
+        {
+            int nyttSpelformID = 0;
+            bool kollaOK = true;
+            string sql;
+
+            if (kollaOK)
+            {
+                SpelformData SpelformData = new SpelformData();
+                if (nySpelform)
+                {
+                    SpelformData.SparaNySpelform(Spelform, ref felID, ref feltext);
+                    nyttSpelformID = Convert.ToInt32(SpelformData.HämtaMaxSpelform());
+                    Spelform.SpelformID = nyttSpelformID;
+                }
+                else
+                {
+                    SpelformData.SparaSpelform(Spelform, ref felID, ref feltext);
+                }
+            }
+            else
+            {
+                throw new HookerException();
+            }
+
+            return nyttSpelformID;
+        }
+
+        /// <summary>
+        /// Ta bort Spelform i databasen 
+        /// </summary>
+        /// <param name="Spelform">Aktuell Spelform</param>
+        /// <param name="felID">Felmeddelande i Ordlistan som ska visas</param>
+        /// <param name="feltext">Ev kompletterande felmeddelande som returneras</param>
+        public void TaBort(Spelform Spelform, ref string felID, ref string feltext)
+        {
+            SpelformData SpelformData = new SpelformData();
+            SpelformData.TabortSpelform(Spelform, ref felID, ref feltext);
+        }
+    }
+}
---
+++ Datalager/SpelformData.cs
@@ -0,0 +1,256 @@
+﻿using GemensamService;
+using Hooker.Affärsobjekt;
+using Hooker.Dataset;
+using Hooker.Gemensam;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Hooker.Datalager
+{
+    /// <summary>
+    /// Datalagerklass för Spelform
+    /// </summary>
+    public sealed class SpelformData : AbstractDataLager
+    {
+        /// <summary>
+        /// Hämtar rad från tabellen Spelform i aktuell databas med angiven nyckel.
+        /// </summary>
+        /// <param name="SpelformID">Aktuell spelform</param>
+        /// <returns>Typat dataset med efterfrågat data</returns>
+        public SpelformDS HämtaSpelform(int SpelformID)
+        {
+            SpelformDS ds = new SpelformDS();
+            string sql;
+
+            try
+            {
+                ds.EnforceConstraints = false;
+                sql = "SELECT s.* FROM Spelform s WHERE s.SpelformID = @SpelformID";
+                List<DatabasParameters> dbParameters = new List<DatabasParameters>()
+                {
+                    new DatabasParameters("@SpelformID", DataTyp.Int, SpelformID.ToString())
+                };
+                DatabasAccess.FyllEnkeltDataSet(sql, dbParameters, ds);
+                return ds;
+            }
+            catch (HookerException hex)
+            {
+                throw hex;
+            }
+            finally
+            {
+                if (DatabasAccess != null)
+                {
+                    DatabasAccess.Dispose();
+                }
+            }
+        }
+        /// <summary>
+        /// Hämtar alla rader från tabellen Spelform i aktuell databas.
+        /// </summary>
+        /// <returns>Typat dataset med efterfrågat data</returns>
+        public DataSet HämtaAllaSpelformer()
+        {
+            SpelformDS ds = new SpelformDS();
+            string sql;
+
+            try
+            {
+                ds.EnforceConstraints = false;
+                sql = "SELECT s.* FROM Spelform s ORDER BY s.Beskrivning";
+                DatabasAccess.FyllEnkeltDataSet(sql, ds);
+                return ds;
+            }
+            catch (HookerException hex)
+            {
+                throw hex;
+            }
+            finally
+            {
+                if (DatabasAccess != null)
+                {
+                    DatabasAccess.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Hämtar rad från tabellen Spelform i aktuell databas.
+        /// </summary>
+        /// <returns>Typat dataset med efterfrågat data</returns>
+        public string HämtaMaxSpelform()
+        {
+            DataSet SpelformDS = new DataSet();
+            string nyttSpelformID = string.Empty;
+            string sql;
+
+            try
+            {
+                sql = "SELECT s.SpelformID FROM Spelform s " +
+                    " ORDER BY s.SpelformID DESC";
+                SpelformDS = DatabasAccess.RunSql(sql);
+            }
+            catch (HookerException hex)
+            {
+                throw hex;
+            }
+            finally
+            {
+                if (DatabasAccess != null)
+                {
+                    DatabasAccess.Dispose();
+                }
+            }
+            nyttSpelformID = SpelformDS.Tables[0].Rows[0]["SpelformID"].ToString();
+            return nyttSpelformID;
+        }
+
+        /// <summary>
+        /// Ny Spelform.
+        /// </summary>
+        /// <param name="Spelform">Spelform</param>
+        /// <param name="felID">Felmeddelande i Ordlistan som ska visas</param>
+        /// <param name="feltext">Ev kompletterande felmeddelande som returneras</param>
+        public void SparaNySpelform(Spelform Spelform, ref string felID, ref string feltext)
+        {
+            string sql;
+
+            try
+            {
+                DatabasAccess.SkapaTransaktion();
+                sql = "INSERT INTO Spelform(Titel, Beskrivning, Lagspel, AntalPerLag, AnvandarNamnSkapad, SkapadDatum, AnvandarNamnUppdat, UppdatDatum) " +
+                    "VALUES " +
+                    "(@Titel, @Beskrivning, @Lagspel, @AntalPerLag, @AnvandarNamnSkapad, @SkapadDatum, @AnvandarNamnUppdat, @UppdatDatum)";
+                List<DatabasParameters> dbParameters = new List<DatabasParameters>()
+                {
+                    new DatabasParameters("@Titel", DataTyp.VarChar, Spelform.Titel.ToString()),
+                    new DatabasParameters("@Beskrivning", DataTyp.VarChar, Spelform.Beskrivning.ToString()),
+                    new DatabasParameters("@Lagspel", DataTyp.VarChar, Spelform.Lagspel.ToString()),
+                    new DatabasParameters("@AntalPerLag", DataTyp.VarChar, Spelform.AntalPerLag.ToString()),
+                    new DatabasParameters("@AnvandarNamnSkapad", DataTyp.VarChar, Spelform.AnvandarNamnSkapad.ToString()),
+                    new DatabasParameters("@SkapadDatum", DataTyp.VarChar, Spelform.SkapadDatum.ToString()),
+                    new DatabasParameters("@AnvandarNamnUppdat", DataTyp.VarChar, Spelform.AnvandarNamnUppdat.ToString()),
+                    new DatabasParameters("@UppdatDatum", DataTyp.VarChar, Spelform.UppdatDatum.ToString())
+                };
+                DatabasAccess.RunSql(sql, dbParameters);
+                DatabasAccess.BekräftaTransaktion();
+            }
+            catch (HookerException hex)
+            {
+                felID = "SQLERROR";
+                feltext = hex.Message.ToString();
+                if (DatabasAccess.HarAktivTransaktion())
+                {
+                    DatabasAccess.ÅngraTransaktion();
+                }
+                throw hex;
+            }
+            catch (Exception ex)
+            {
+                if (DatabasAccess.HarAktivTransaktion())
+                {
+                    DatabasAccess.ÅngraTransaktion();
+                }
+                throw ex;
+            }
+            finally
+            {
+                if (DatabasAccess != null)
+                {
+                    DatabasAccess.Dispose();
+                }
+            }
+            //return nyttBokningID;
+        }
+
+        /// <summary>
+        /// Sparar i Spelform.
+        /// </summary>
+        /// <param name="Spelform">Spelform</param>
+        /// <param name="felID">Felmeddelande i Ordlistan som ska visas</param>
+        /// <param name="feltext">Ev kompletterande felmeddelande som returneras</param>
+        public void SparaSpelform(Spelform Spelform, ref string felID, ref string feltext)
+        {
+            string sql;
+
+            try
+            {
+                DatabasAccess.SkapaTransaktion();
+                sql = "UPDATE Spelform " +
+                    "SET Titel = @Titel, Beskrivning = @Beskrivning, Lagspel = @Lagspel, AntalPerLag = @AntalPerLag, " +
+                    "AnvandarNamnSkapad = @AnvandarNamnSkapad, AnvandarNamnUppdat = @AnvandarNamnUppdat, UppdatDatum = @UppdatDatum " +
+                    "WHERE SpelformID = @SpelformID";
+                List<DatabasParameters> dbParameters = new List<DatabasParameters>()
+                {
+                    new DatabasParameters("@SpelformID", DataTyp.Int, Spelform.SpelformID.ToString()),
+                    new DatabasParameters("@Titel", DataTyp.VarChar, Spelform.Titel.ToString()),
+                    new DatabasParameters("@Beskrivning", DataTyp.VarChar, Spelform.Beskrivning.ToString()),
+                    new DatabasParameters("@Lagspel", DataTyp.VarChar, Spelform.Lagspel.ToString()),
+                    new DatabasParameters("@AntalPerLag", DataTyp.VarChar, Spelform.AntalPerLag.ToString()),
+                    new DatabasParameters("@AnvandarNamnSkapad", DataTyp.VarChar, Spelform.AnvandarNamnSkapad.ToString()),
+                    new DatabasParameters("@SkapadDatum", DataTyp.VarChar, Spelform.SkapadDatum.ToString()),
+                    new DatabasParameters("@AnvandarNamnUppdat", DataTyp.VarChar, Spelform.AnvandarNamnUppdat.ToString()),
+                    new DatabasParameters("@UppdatDatum", DataTyp.VarChar, Spelform.UppdatDatum.ToString())
+                };
+                DatabasAccess.RunSql(sql, dbParameters);
+                DatabasAccess.BekräftaTransaktion();
+            }
+            catch (HookerException hex)
+            {
+                felID = "SQLERROR";
+                feltext = hex.Message.ToString();
+                DatabasAccess.ÅngraTransaktion();
+                throw hex;
+            }
+            catch (Exception ex)
+            {
+                DatabasAccess.ÅngraTransaktion();
+                throw ex;
+            }
+            finally
+            {
+                DatabasAccess.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Ta bort Spelform.
+        /// </summary>
+        /// <param name="Spelform">Spelform</param>
+        /// <param name="felID">Felmeddelande i Ordlistan som ska visas</param>
+        /// <param name="feltext">Ev kompletterande felmeddelande som returneras</param>
+        public void TabortSpelform(Spelform Spelform, ref string felID, ref string feltext)
+        {
+            string sql;
+            DatabasAccess.SkapaTransaktion();
+
+            try
+            {
+                sql = "DELETE FROM Spelform WHERE SpelformID = @SpelformID";
+                List<DatabasParameters> dbParameters = new List<DatabasParameters>()
+            {
+                new DatabasParameters("@SpelformID", DataTyp.Int, Spelform.SpelformID.ToString())
+            };
+                DatabasAccess.RunSql(sql, dbParameters);
+                DatabasAccess.BekräftaTransaktion();
+            }
+            catch (HookerException hex)
+            {
+                felID = "SQLERROR";
+                feltext = hex.Message.ToString();
+                DatabasAccess.ÅngraTransaktion();
+                throw hex;
+            }
+            catch (Exception ex)
+            {
+                DatabasAccess.ÅngraTransaktion();
+                throw ex;
+            }
+            finally
+            {
+                DatabasAccess.Dispose();
+            }
+        }
+    }
+}
